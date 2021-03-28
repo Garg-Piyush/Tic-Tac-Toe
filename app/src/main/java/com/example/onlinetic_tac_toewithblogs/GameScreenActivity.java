e030f86ed982250ca44fb95c64482e6ae386730b
Piyush Garg <39gargpiyush@gmail.com>
2021-03-29 01:02:22 +0530

    Game UI

---
+++ app/src/main/java/com/example/onlinetic_tac_toewithblogs/GameScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.onlinetic_tac_toewithblogs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_screen);
+    }
+}
